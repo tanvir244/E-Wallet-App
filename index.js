8e3fe013e1ab0ea2512c8dd8713cf0b1701aa243
tanvir243 <travelertanvir@gmail.com>
2024-03-15 04:07:41 +0600

    showing dynamic time completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// UI 
 document.querySelector('#ewallet-form').addEventListener('submit', function (e) {
     e.preventDefault();
     console.log('submitted!');
@@ -7,16 +8,14 @@ document.querySelector('#ewallet-form').addEventListener('submit', function (e) 
     const desc = document.querySelector('.add__description').value;
     const value = document.querySelector('.add__value').value;
 
-    if(desc.length > 0 && value.length > 0){
+    if (desc.length > 0 && value.length > 0) {
         addItems(type, desc, value);
         resetForm();
     }
-
-    console.log(type, desc, value);
-
 });
 
 function addItems(type, desc, value) {
+    const time = getFormattedTime();
     const newHtml = `
     <div class="item">
           <div class="item-description-time">
@@ -24,7 +23,7 @@ function addItems(type, desc, value) {
               <p>${desc}</p>
             </div>
             <div class="item-time">
-              <p>25 Feb, 06:45 PM</p>
+              <p>${time}</p>
             </div>
           </div>
           <div class="item-amount ${type === '+' ? 'income-amount' : 'expense-amount'}">
@@ -43,3 +42,16 @@ function resetForm() {
     document.querySelector('.add__value').value = '';
 }
 
+// Utility Functions 
+function getFormattedTime() {
+    const now = new Date().toLocaleTimeString('en-us', {
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+    // Mar 15, 03:49 AM
+    const date = now.split(',')[0].split(' ');
+    const time = now.split(',')[1];
+    return `${date[1]} ${date[0]}, ${time}`;
+}
